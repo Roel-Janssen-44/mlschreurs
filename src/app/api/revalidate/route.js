ec30184d45206cb663326cc235ef9806ab728eb0
Websidesign <service@websidesign.nl>
2023-10-19 11:07:32 +0200

    main

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
 export async function POST() {
-  // Verify the request comes from Prismic (optional but recommended)
-  const prismicSecret = process.env.PRISMIC_WEBHOOK_SECRET;
-  const webhookSecret = request.headers.get("Prismic-Secret");
-  if (prismicSecret && webhookSecret !== prismicSecret) {
-    return NextResponse.error("Unauthorized", { status: 401 });
-  }
+  // // Verify the request comes from Prismic (optional but recommended)
+  // const prismicSecret = process.env.PRISMIC_WEBHOOK_SECRET;
+  // const webhookSecret = request.headers.get("Prismic-Secret");
+  // if (prismicSecret && webhookSecret !== prismicSecret) {
+  //   return NextResponse.error("Unauthorized", { status: 401 });
+  // }
   revalidateTag("prismic");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
