0199294c7b0449946790a3a6c0e4f44ac0ad19b7
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2023-11-10 13:19:23 +0100

    column 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ export default function Footer({ data }) {
             </ul>
           </div>
         </div>
-        {/* <div
+        <div
           className={`w-full text-center flex justify-center mb-8 md:justify-normal md:text-left md:w-1/2 lg:justify-center lg:w-1/3`}
         >
           <div className="">
@@ -46,7 +46,7 @@ export default function Footer({ data }) {
               ))}
             </ul>
           </div>
-        </div> */}
+        </div>
         {/* <div
           className={`w-full text-center flex justify-center mb-8 md:justify-end md:text-left md:w-1/2 lg:justify-center lg:w-1/3`}
         >
